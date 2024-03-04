a92d437c40451dcee8d34d370b9a98caf0a6180a
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-04 15:09:50 +0100

    Change directory of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from "react";
-import Header from "./assets/components/Header";
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
 import Score from "./Score";
 import Player from "./Player";
-import Button from "./assets/components/Button";
-import Wrapper from "./assets/components/Wrapper";
+import Button from "./components/Button";
+import Wrapper from "./components/Wrapper";
+import Modal from "./components/Modal";
+
 const App = () => {
   const [playerScore, setPlayerScore] = useState(0);
   const [computerScore, setComputerScore] = useState(0);
-  const [choice,setChoice] = useState("?")
-  const [computerChoice,setComputerChoice] = useState("?")
-
+  const [choice, setChoice] = useState("?");
+  const [computerChoice, setComputerChoice] = useState("?");
   const [text, setText] = useState("Choose your weapon");
   const [information, setInformation] = useState(
     "First to 5 points wins the game !"
   );
-
   const options = ["✊", "✋", "✌"];
+
+  useEffect(() => {
+    checkForWinner(playerScore, computerScore);
+    console.log("pozvano");
+  }, [playerScore, computerScore]);
 
   const callAction = (e) => {
     const playerChoice = e.target.textContent;
     const random = Math.floor(Math.random() * options.length);
     const computerChoice = options[random];
-    setChoice(playerChoice)
-    setComputerChoice(computerChoice)
+    setChoice(playerChoice);
+    setComputerChoice(computerChoice);
     console.log(playerChoice, computerChoice);
-    checkResult(playerChoice, computerChoice);
+    console.log(playerScore, computerScore);
+    checkRoundWinner(playerChoice, computerChoice);
   };
 
-  const checkResult = (player, computer) => {
+  const checkRoundWinner = (player, computer) => {
     if (player === "✊" && computer === "✊") {
       console.log("nerijeseno");
       setText("It's a tie !");
@@ -42,48 +48,88 @@ const App = () => {
       setInformation("Paper ties with Paper");
     } else if (player === "✊" && computer === "✋") {
       console.log("Komjuter dobio");
-      setComputerScore((prev) => (prev += 1));
+      setComputerScore((prev) => {
+        prev += 1;
+        // checkForWinner(playerScore, computerScore);
+        return prev;
+      });
+
       setText("Computer won!");
       setInformation("Paper beats rock");
     } else if (player === "✊" && computer === "✌") {
       console.log("Plejer dobio");
-      setPlayerScore((prev) => (prev += 1));
+      setPlayerScore((prev) => {
+        prev += 1;
+        // checkForWinner(playerScore, computerScore);
+        return prev;
+      });
+
       setText("You won!");
       setInformation("Rock beats scissors");
     } else if (player === "✋" && computer === "✊") {
       console.log("plejer dobio");
-      setPlayerScore((prev) => (prev += 1));
+      setPlayerScore((prev) => {
+        prev += 1;
+        // checkForWinner(playerScore, computerScore);
+        return prev;
+      });
+
       setText("You won!");
       setInformation("Paper beats rock");
     } else if (player === "✋" && computer === "✌") {
       console.log("Kompjuter dobio");
-      setComputerScore((prev) => (prev += 1));
+      setComputerScore((prev) => {
+        prev += 1;
+        // checkForWinner(playerScore, computerScore);
+        return prev;
+      });
+
       setText("Computer won!");
       setInformation("Scissors beat paper");
     } else if (player === "✌" && computer === "✋") {
       console.log("Plejer dobio");
-      setPlayerScore((prev) => (prev += 1));
+      setPlayerScore((prev) => {
+        prev += 1;
+        // checkForWinner(playerScore, computerScore);
+        return prev;
+      });
+
       setText("You won");
       setInformation("Scissors beat paper");
     } else if (player === "✌" && computer === "✊") {
       console.log("kompjuter dobio");
-      setComputerScore((prev) => (prev += 1));
+      setComputerScore((prev) => {
+        prev += 1;
+        // checkForWinner(playerScore, computerScore);
+        return prev;
+      });
       setText("Computer won");
       setInformation("Rock beats scissors");
     }
   };
 
-  // napravim 3 objekta u array name:"scissors", emoji:taj emoji, id:random
+  const checkForWinner = (a, b) => {
+    if (a === 3) {
+      return console.log("A won");
+    } else if (b === 3) {
+      return console.log("B won");
+    }
+  };
 
-  const roundWinner = (player) => {};
+  // napravim 3 objekta u array name:"scissors", emoji:taj emoji, id:random
 
   return (
     <>
       <Header />
       <Score text={text} information={information} />
+      <Modal />
       <Wrapper>
-        <Player name={"Player"} score={playerScore} choice={choice}/>
-        <Player name={"Computer"} score={computerScore} choice={computerChoice}/>
+        <Player name={"Player"} score={playerScore} choice={choice} />
+        <Player
+          name={"Computer"}
+          score={computerScore}
+          choice={computerChoice}
+        />
       </Wrapper>
       <Wrapper>
         {options.map((option, index) => (
---
+++ src/components/Modal.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Modal = () => {
+  return (
+    <div className='fixed top-0 left-0 w-screen h-scren blur-lg z-10 bg-gray-500'>
+      
+    </div>
+  )
+}
+// position: fixed;
+//     top: 0;
+//     left: 0;
+//     width: 100%;
+//     height: 100vh;
+//     filter: blur(0.3rem);
+//     z-index: 10;
+//     background-color: rgba(0, 0, 0, 0.6);
+export default Modal
