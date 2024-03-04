676329e0505db34d55f45f4989faa2969d5aa0eb
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-04 12:53:29 +0100

    Add rules

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,38 @@ import Player from "./Player";
 import Button from "./assets/components/Button";
 import Wrapper from "./assets/components/Wrapper";
 const App = () => {
+  const options = ["✊", "✋", "✌"];
+
   const chooseFunction = (e) => {
-    console.dir(e.target.textContent);
-    const choice = e.target.textContent;
-    return choice;
+    const playerChoice = e.target.textContent;
+    // treba da uzme rezultat,
+    const random = Math.floor(Math.random() * options.length);
+    // da pozove random rezultat iz arraja
+    const computerChoice = options[random];
+    console.log(playerChoice, computerChoice)
+
+    checkResult(playerChoice, computerChoice);
+    // da uporedi oba rezultata
+    // ako ima pobjednika da dodijeli pravom poen
   };
 
-  const options = ["✊", "✋", "✌"];
+  const checkResult = (player, computer) => {
+    if (player === computer) {
+      console.log("nerijeseno");
+    } else if (player === "✊" && computer === "✋") {
+      console.log("Komjuter dobio");
+    } else if (player === "✊" && computer === "✌") {
+      console.log("Plejer dobio");
+    } else if (player === "✋" && computer === "✊") {
+      console.log("plejer dobio");
+    } else if (player === "✋" && computer === "✌") {
+      console.log("Kompjuter dobio");
+    } else if (player === "✌" && computer === "✋") {
+      console.log("Plejer dobio");
+    } else if (player === "✌" && computer === "✊") {
+      console.log("kompjuter dobio");
+    }
+  };
 
   return (
     <>
